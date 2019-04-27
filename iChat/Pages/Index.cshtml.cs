7c6ed4be3cc1305fb2b1161aa69605b98a508239
liefu zhang <liefuzhangnz@gmail.com>
2019-04-27 21:36:58 +1200

    Display channels on page

--- iChat/Pages/Index.cshtml.cs
+++ iChat/Pages/Index.cshtml.cs
@@ -2,16 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using iChat.Data;
+using iChat.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace iChat.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly iChatContext _context;
 
+        public IndexModel(iChatContext context) {
+            _context = context;
+        }
+
+        public IList<Channel> Channels { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Channels = await _context.Channels.AsNoTracking().ToListAsync();
         }
     }
 }
