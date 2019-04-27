e6278e00bc9b009b79f7f641e5150d8588417656
liefu zhang <liefuzhangnz@gmail.com>
2019-04-27 18:53:37 +1200

    Initial commit; Added Channel model

---
+++ iChat/Data/DbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iChat.Models;
+
+namespace iChat.Data {
+    public static class DbInitializer {
+        public static void Initialize(iChatContext context) {
+            //context.Database.EnsureCreated();
+
+            if (context.Channels.Any()) {
+                return; // DB has been seeded
+            }
+
+            var channels = new Channel[] {
+                new Channel {Name= "general", Topic = "Anything that's talkable"},
+                new Channel {Name= "random", Topic = "Another random channel"}
+            };
+            context.Channels.AddRange(channels);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ iChat/Data/iChatContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iChat.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace iChat.Data
+{
+    public class iChatContext : DbContext {
+        public iChatContext(DbContextOptions<iChatContext> options) : base(options) {
+        }
+
+        public DbSet<Channel> Channels { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Channel>().ToTable("Channel");
+        }
+    }
+}
