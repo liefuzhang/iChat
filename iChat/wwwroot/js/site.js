3c6cc2bcf4f1bcefe8ff256d22e8843b7e1e6ac9
liefu <liefu@flowingly.net>
2019-05-01 17:03:10 +1200

    Handle keydown event in editor

--- iChat/wwwroot/js/site.js
+++ iChat/wwwroot/js/site.js
@@ -3,92 +3,79 @@
 var mainController = (function () {
     var quill;
 
-    function toggleFormatChars(range, char) {
-        var text = quill.getText(range.index, range.length);
+    function toggleFormatChars(char) {
+        var selection = quill.getSelection();
+        var length = selection.length;
+        if (length < 1)
+            return;
+
+        var start = selection.index;
+        var text = quill.getText(start, length);
         var newText;
         if (text.length > 2 && text.startsWith(char) && text.endsWith(char)) {
             // remove char
-            newText = text.substr(1, range.length - 2);
-            quill.deleteText(range.index, range.length);
-            quill.insertText(range.index, newText);
-            quill.setSelection(range.index, range.length - 2);
+            newText = text.substr(1, length - 2);
+            quill.deleteText(start, length);
+            quill.insertText(start, newText);
+            quill.setSelection(start, length - 2);
         } else {
             // add char
             newText = char + text + char;
-            quill.deleteText(range.index, range.length);
-            quill.insertText(range.index, newText);
-            quill.setSelection(range.index, range.length + 2);
+            quill.deleteText(start, length);
+            quill.insertText(start, newText);
+            quill.setSelection(start, length + 2);
         }
     }
 
-    //var bindings = {
-    //    enter: {
-    //        key: 'enter',
-    //        shiftKey: false,
-    //        handler: function () {
-    //            var message = quill.root.innerHTML;
-    //            if (!message)
-    //                return false;
-
-    //            var form = document.querySelector('#messageForm');
-    //            var messageInput = document.querySelector('input[name=newMessage]');
-    //            messageInput.value = message;
-    //            form.submit();
-    //            return false;
-    //        }
-    //    },
-    //    custom_bold: {
-    //        key: 'B',
-    //        shortKey: true,
-    //        handler: function (range, context)
-    //        {
-    //            if (range.length < 1)
-    //                return false;
-
-    //            toggleFormatChars(range, '*');
-
-    //            return false;
-    //        }
-    //    },
-    //    bold: {
-    //        key: 'I',
-    //        shortKey: true,
-    //        handler: function (range, context) {
-    //            if (range.length < 1)
-    //                return false;
-
-    //            toggleFormatChars(range, '_');
-
-    //            return false;
-    //        }
-    //    }
-    //};
-
     function init(channelName) {
+        var messageScrollable = document.querySelector(".message-scrollable");
+        messageScrollable.scrollTop = messageScrollable.scrollHeight;
+        messageScrollable.style.visibility = 'visible'; // show now to avoid flickering
+
+        var bindings = {
+            list: {
+                key: 'enter',
+                shiftKey: false,
+                handler: function () {
+                    var message = quill.root.innerHTML;
+                    if (!message)
+                        return false;
+                    var form = document.querySelector('#messageForm');
+                    var messageInput = document.querySelector('input[name=newMessage]');
+                    messageInput.value = message;
+                    form.submit();
+                    return false;
+                }
+            }
+        };
+
         quill = new Quill('#messageEditor',
             {
                 theme: 'snow',
                 placeholder: `Message ${channelName}`,
-                //modules: {
-                //    keyboard: {
-                //        bindings: bindings
-                //    }
-                //}
+                modules: {
+                    keyboard: {
+                        bindings: bindings
+                    }
+                }
             });
 
         var messageBox = document.querySelector('.message-box');
         messageBox.addEventListener("keydown",
             function(event) {
                 var ret = true;
+                var char ='';
                 if (event.ctrlKey || event.metaKey) {
                     switch (event.keyCode) {
                         case 66: // ctrl+B or ctrl+b
                         case 98:
                             ret = false;
+                            char = '*';
                             break;
                         case 73: // ctrl+I or ctrl+i
                         case 105:
                             ret = false;
+                            char = '_';
                             break;
                         case 85: // ctrl+U or ctrl+u
                         case 117:
@@ -96,6 +83,10 @@ var mainController = (function () {
                             break;
                     }
                 }
+                if (!!char) {
+                    toggleFormatChars(char);
+                }
+
                 if (ret === false) {
                     event.stopPropagation();
                     event.preventDefault();
