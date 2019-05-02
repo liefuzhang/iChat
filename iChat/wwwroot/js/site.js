ee77c2875abf0bfe1a13f640f7f422fe11b910e3
liefu zhang <liefuzhangnz@gmail.com>
2019-05-02 22:20:38 +1200

    clear formats on paste

--- iChat/wwwroot/js/site.js
+++ iChat/wwwroot/js/site.js
@@ -2,32 +2,8 @@
 
 var mainController = (function () {
     var quill;
-
-    function toggleFormatChars(char) {
-        var selection = quill.getSelection();
-        var length = selection.length;
-        if (length < 1)
-            return;
-
-        var start = selection.index;
-        var text = quill.getText(start, length);
-        var newText;
-        if (text.length > 2 && text.startsWith(char) && text.endsWith(char)) {
-            // remove char
-            newText = text.substr(1, length - 2);
-            quill.deleteText(start, length);
-            quill.insertText(start, newText);
-            quill.setSelection(start, length - 2);
-        } else {
-            // add char
-            newText = char + text + char;
-            quill.deleteText(start, length);
-            quill.insertText(start, newText);
-            quill.setSelection(start, length + 2);
-        }
-    }
-
-    function init(channelName) {
+    
+    function initSimpleBar() {
         var messageScrollable = document.querySelector(".message-scrollable");
 
         // init simplebar
@@ -36,6 +12,28 @@ var mainController = (function () {
         var scrollable = document.querySelector(".simplebar-content");
         scrollable.scrollTop = scrollable.scrollHeight;
         messageScrollable.style.visibility = 'visible'; // show now to avoid flickering
+    }
+
+    function configPlainClipboard() {
+        var Clipboard = Quill.import('modules/clipboard');
+        var Delta = Quill.import('delta');
+
+        class PlainClipboard extends Clipboard {
+            convert(html = null) {
+                if (typeof html === 'string') {
+                    this.container.innerHTML = html;
+                }
+                let text = this.container.innerText;
+                this.container.innerHTML = '';
+                return new Delta().insert(text);
+            }
+        }
+
+        Quill.register('modules/clipboard', PlainClipboard, true);
+    }
+
+    function initQuill(channelName) {
+        configPlainClipboard();
 
         var bindings = {
             list: {
@@ -64,12 +62,38 @@ var mainController = (function () {
                     }
                 }
             });
+    }
 
+    function toggleFormatChars(char) {
+        var selection = quill.getSelection();
+        var length = selection.length;
+        if (length < 1)
+            return;
+
+        var start = selection.index;
+        var text = quill.getText(start, length);
+        var newText;
+        if (text.length > 2 && text.startsWith(char) && text.endsWith(char)) {
+            // remove char
+            newText = text.substr(1, length - 2);
+            quill.deleteText(start, length);
+            quill.insertText(start, newText);
+            quill.setSelection(start, length - 2);
+        } else {
+            // add char
+            newText = char + text + char;
+            quill.deleteText(start, length);
+            quill.insertText(start, newText);
+            quill.setSelection(start, length + 2);
+        }
+    }
+
+    function captureKeydownEvent() {
         var messageBox = document.querySelector('.message-box');
         messageBox.addEventListener("keydown",
-            function(event) {
+            function (event) {
                 var ret = true;
-                var char ='';
+                var char = '';
                 if (event.ctrlKey || event.metaKey) {
                     switch (event.keyCode) {
                         case 66: // ctrl+B or ctrl+b
@@ -100,6 +124,12 @@ var mainController = (function () {
 
                 return ret;
             }, true); // true - event capturing phase
+    }
+
+    function init(channelName) {
+        initSimpleBar();
+        initQuill(channelName);
+        captureKeydownEvent();
     }
 
     return {
