6578639771fc835d23bba3fc33265cd0be2af052
liefu zhang <liefuzhangnz@gmail.com>
2019-05-11 15:11:35 +1200

    Added create workspace button

--- ichat.react/src/account/Login.js
+++ ichat.react/src/account/Login.js
@@ -24,36 +24,42 @@ class Login extends React.Component {
     return (
       <div className="login-page">
         <div className="login-container panel">
-          <h1>Login</h1>
-          <div className="login-form">
-            <form id="account" method="post">
-              <p>Enter email address and password to log in.</p>
-              <input
-                className="form-control"
-                name="email"
-                type="email"
-                placeholder="Email"
-                required
-              />
-              <input
-                className="form-control"
-                name="password"
-                type="password"
-                placeholder="Password"
-                required
-              />
-              <button type="submit" className="btn form-control">
-                Log in
-              </button>
-              <div className="remember-me">
-                <input name="rememberMe" type="checkbox" />
-                <label>Remember me</label>
-              </div>
-              <div className="forgot-password">
-                <a>Forgot your password?</a>
-              </div>
-            </form>
-          </div>
+          <section className="login-section">
+            <h1 style={{textAlign: 'center'}}>Login to your workspace</h1>
+            <div className="login-form">
+              <form id="account" method="post">
+                <p>Enter email address and password to log in.</p>
+                <input
+                  className="form-control"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  required
+                />
+                <input
+                  className="form-control"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  required
+                />
+                <button type="submit" className="btn form-control">
+                  Log in
+                </button>
+                <div className="remember-me">
+                  <input name="rememberMe" type="checkbox" />
+                  <label>Remember me</label>
+                </div>
+                <div className="forgot-password">
+                  <a>Forgot your password?</a>
+                </div>
+              </form>
+            </div>
+          </section>
+          <div className="horizontal-divider">or</div>
+          <section className="login-section">
+            <button className="btn white-btn">Create New WorkSpace</button>
+          </section>
         </div>
       </div>
     );
