52239698b716f99ddda7663ee087b5f865bc926e
liefu zhang <liefuzhangnz@gmail.com>
2019-05-16 21:40:31 +1200

    Adding modal

--- ichat.react/src/SidebarHeader.js
+++ ichat.react/src/SidebarHeader.js
@@ -11,6 +11,8 @@ class SidebarHeader extends React.Component {
     document.querySelector(".sidebar-header-dropdown").classList.toggle("hide");
   }
 
+  onInvitePeople(event) {}
+
   componentDidMount() {
     document
       .querySelector(".sidebar-header-container")
@@ -25,37 +27,57 @@ class SidebarHeader extends React.Component {
 
   render() {
     return (
-      <div className="sidebar-header">
-        <div className="sidebar-header-container">
-          <div>
-            <span className="sidebar-header-workspace-name">
-              {this.props.userProfile.workspaceName}{" "}
-            </span>
-            <FontAwesomeIcon icon="chevron-down" />
-          </div>
-          <div className="sidebar-header-user-name">
-            {this.props.userProfile.displayName}
-          </div>
-        </div>
-        <div className="sidebar-header-dropdown panel hide">
-          <section className="sidebar-header-dropdown-section">
-            <div className="sidebar-header-dropdown-section-header">
+      <div>
+        <div className="sidebar-header">
+          <div className="sidebar-header-container">
+            <div>
+              <span className="sidebar-header-workspace-name">
+                {this.props.userProfile.workspaceName}{" "}
+              </span>
+              <FontAwesomeIcon icon="chevron-down" />
+            </div>
+            <div className="sidebar-header-user-name">
               {this.props.userProfile.displayName}
             </div>
-            <ul>
-              <li>Set status</li>
-              <li>Profile</li>
-            </ul>
-          </section>
-          <section className="sidebar-header-dropdown-section">
-            <div className="sidebar-header-dropdown-section-header">
-              {this.props.userProfile.workspaceName}
+          </div>
+          <div className="sidebar-header-dropdown panel hide">
+            <section className="sidebar-header-dropdown-section">
+              <div className="sidebar-header-dropdown-section-header">
+                {this.props.userProfile.displayName}
+              </div>
+              <ul>
+                <li>Set status</li>
+                <li>Profile</li>
+              </ul>
+            </section>
+            <section className="sidebar-header-dropdown-section">
+              <div className="sidebar-header-dropdown-section-header">
+                {this.props.userProfile.workspaceName}
+              </div>
+              <ul>
+                <li onClick={this.onInvitePeople}>Invite people</li>
+                <li>Log out of {this.props.userProfile.workspaceName}</li>
+              </ul>
+            </section>
+          </div>
+        </div>
+        <div className="modal-panel-container">
+          <div className="modal-panel-overlay">
+            <div className="modal-panel panel">
+              <h1 style={{ textAlign: "center" }}>
+                Invite People to {this.props.userProfile.workspaceName}
+              </h1>
+              <div className="invite-people">
+                <input
+                  className="form-control"
+                  type="email"
+                  placeholder="Email"
+                  required
+                />
+                <button className="btn form-control">Send Invitation</button>
+              </div>
             </div>
-            <ul>
-              <li>Invite people</li>
-              <li>Log out of {this.props.userProfile.workspaceName}</li>
-            </ul>
-          </section>
+          </div>
         </div>
       </div>
     );
