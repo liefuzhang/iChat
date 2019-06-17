acaa6da237f24af2853cbff6f1fe13e1a6a1e068
liefu zhang <liefuzhangnz@gmail.com>
2019-06-18 00:01:26 +1200

    Adding file download

--- iChat.Api/Helpers/IFileHelper.cs
+++ iChat.Api/Helpers/IFileHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
@@ -7,5 +8,6 @@ namespace iChat.Api.Helpers
     public interface IFileHelper
     {
         Task<string> UploadFileAsync(IFormFile file, int workspaceId);
+        Task<FileStream> DownloadFileAsync(string savedName, int workspaceId);
     }
 }
--- iChat.Api/Services/MessageService.cs
+++ iChat.Api/Services/MessageService.cs
@@ -8,13 +8,12 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace iChat.Api.Services
-{
-    public class MessageService : IMessageService
-    {
+namespace iChat.Api.Services {
+    public class MessageService : IMessageService {
         private readonly iChatContext _context;
         private readonly IChannelService _channelService;
         private readonly IMessageParsingHelper _messageParsingHelper;
@@ -22,8 +21,7 @@ namespace iChat.Api.Services
         private readonly IFileHelper _fileHelper;
 
         public MessageService(iChatContext context, IChannelService channelService,
-            IMessageParsingHelper messageParsingHelper, IMapper mapper, IFileHelper fileHelper)
-        {
+            IMessageParsingHelper messageParsingHelper, IMapper mapper, IFileHelper fileHelper) {
             _context = context;
             _channelService = channelService;
             _messageParsingHelper = messageParsingHelper;
@@ -31,13 +29,11 @@ namespace iChat.Api.Services
             _fileHelper = fileHelper;
         }
 
-        private async Task<List<MessageGroupDto>> GetMessageGroups(IQueryable<Message> baseQuery)
-        {
+        private async Task<List<MessageGroupDto>> GetMessageGroups(IQueryable<Message> baseQuery) {
             var groups = await baseQuery.GroupBy(cm => cm.CreatedDate.Date)
                 .OrderBy(group => group.Key)
                 .Select(group =>
-                    new MessageGroupDto
-                    {
+                    new MessageGroupDto {
                         DateString = group.Key.ToString("dddd, MMM d", CultureInfo.InvariantCulture),
                         Messages = group.Select(m => _mapper.Map<MessageDto>(m))
                     })
@@ -49,22 +45,17 @@ namespace iChat.Api.Services
             return groups;
         }
 
-        private void AllowConsecutiveMessages(List<MessageGroupDto> groups)
-        {
+        private void AllowConsecutiveMessages(List<MessageGroupDto> groups) {
             var maxDiffInMin = 3;
-            groups.ForEach(g =>
-            {
+            groups.ForEach(g => {
                 var messages = g.Messages.ToList();
-                for (var i = 0; i < messages.Count(); i++)
-                {
-                    if (i == 0 || messages[i - 1].SenderId != messages[i].SenderId)
-                    {
+                for (var i = 0; i < messages.Count(); i++) {
+                    if (i == 0 || messages[i - 1].SenderId != messages[i].SenderId) {
                         continue;
                     }
                     var time = DateTime.Parse(messages[i].TimeString);
                     var prevTime = DateTime.Parse(messages[i - 1].TimeString);
-                    if ((time - prevTime).Minutes <= maxDiffInMin)
-                    {
+                    if ((time - prevTime).Minutes <= maxDiffInMin) {
                         messages[i].IsConsecutiveMessage = true;
                     }
                 }
@@ -72,22 +63,19 @@ namespace iChat.Api.Services
             });
         }
 
-        private async Task AddFilesToMessagesAsync(List<MessageGroupDto> groups)
-        {
+        private async Task AddFilesToMessagesAsync(List<MessageGroupDto> groups) {
             var fileMessages = groups.Select(g => g.Messages).SelectMany(m => m).Where(m => m.HasFileAttachments).ToList();
             var fileMessageIds = fileMessages.Select(m => m.Id);
             var files = await _context.Files.Include(f => f.MessageFileAttachments)
                 .Where(f => f.MessageFileAttachments
                 .Any(mfa => fileMessageIds.Contains(mfa.MessageId))).ToListAsync();
-            foreach (var fileMessage in fileMessages)
-            {
+            foreach (var fileMessage in fileMessages) {
                 var fileAttachments = files.Where(f => f.MessageFileAttachments.Any(mfa => mfa.MessageId == fileMessage.Id)).ToList();
                 fileMessage.FileAttachments = _mapper.Map<List<FileDto>>(fileAttachments);
             }
         }
 
-        public async Task<IEnumerable<MessageGroupDto>> GetMessagesForChannelAsync(int channelId, int workspaceId)
-        {
+        public async Task<IEnumerable<MessageGroupDto>> GetMessagesForChannelAsync(int channelId, int workspaceId) {
             var messageGroupsBaseQuery = _context.ChannelMessages
                 .Include(m => m.Sender)
                 .Where(m => m.ChannelId == channelId && m.WorkspaceId == workspaceId);
@@ -96,8 +84,7 @@ namespace iChat.Api.Services
             return messageGroups;
         }
 
-        public async Task<IEnumerable<MessageGroupDto>> GetMessagesForConversationAsync(int conversationId, int workspaceId)
-        {
+        public async Task<IEnumerable<MessageGroupDto>> GetMessagesForConversationAsync(int conversationId, int workspaceId) {
             var messageGroupsBaseQuery = _context.ConversationMessages
                 .Include(m => m.Sender)
                 .Where(m => m.ConversationId == conversationId && m.WorkspaceId == workspaceId);
@@ -107,8 +94,7 @@ namespace iChat.Api.Services
         }
 
         public async Task<int> PostMessageToConversationAsync(string newMessage, int conversationId, int currentUserId,
-            int workspaceId, bool hasFileAttachments = false)
-        {
+            int workspaceId, bool hasFileAttachments = false) {
             var content = _messageParsingHelper.Parse(newMessage);
             var message = new ConversationMessage(conversationId, content, currentUserId, workspaceId, hasFileAttachments);
 
@@ -118,8 +104,7 @@ namespace iChat.Api.Services
         }
 
         public async Task<int> PostMessageToChannelAsync(string newMessage, int channelId, int currentUserId,
-            int workspaceId, bool hasFileAttachments = false)
-        {
+            int workspaceId, bool hasFileAttachments = false) {
             var content = _messageParsingHelper.Parse(newMessage);
             var message = new ChannelMessage(channelId, content, currentUserId, workspaceId, hasFileAttachments);
 
@@ -129,8 +114,7 @@ namespace iChat.Api.Services
         }
 
         private async Task AddNewFileForMessageAsync(string savedFileName, string fileName, int messageId, int userId,
-            int workspaceId)
-        {
+            int workspaceId) {
             var newFile = new File(savedFileName, fileName, userId, workspaceId);
             _context.Files.Add(newFile);
             await _context.SaveChangesAsync();
@@ -139,19 +123,15 @@ namespace iChat.Api.Services
             await _context.SaveChangesAsync();
         }
 
-        private async Task UploadAndSaveFilesForMessageAsync(IList<IFormFile> files, int messageId, int userId, int workspaceId)
-        {
-            foreach (var file in files)
-            {
+        private async Task UploadAndSaveFilesForMessageAsync(IList<IFormFile> files, int messageId, int userId, int workspaceId) {
+            foreach (var file in files) {
                 var savedFileName = await _fileHelper.UploadFileAsync(file, workspaceId);
                 await AddNewFileForMessageAsync(savedFileName, file.FileName, messageId, userId, workspaceId);
             }
         }
 
-        public async Task PostFileMessageToConversationAsync(IList<IFormFile> files, int conversationId, int userId, int workspaceId)
-        {
-            if (!files.Any())
-            {
+        public async Task PostFileMessageToConversationAsync(IList<IFormFile> files, int conversationId, int userId, int workspaceId) {
+            if (!files.Any()) {
                 return;
             }
 
@@ -160,15 +140,47 @@ namespace iChat.Api.Services
         }
 
 
-        public async Task PostFileMessageToChannelAsync(IList<IFormFile> files, int channelId, int userId, int workspaceId)
-        {
-            if (!files.Any())
-            {
+        public async Task PostFileMessageToChannelAsync(IList<IFormFile> files, int channelId, int userId, int workspaceId) {
+            if (!files.Any()) {
                 return;
             }
 
             var messageId = await PostMessageToChannelAsync(string.Empty, channelId, userId, workspaceId, true);
             await UploadAndSaveFilesForMessageAsync(files, messageId, userId, workspaceId);
+        }
+
+        public async Task<FileStream> DownloadFileAsync(int fileId, int userId, int workspaceId) {
+            if (!(await EligibleForTheFileAsync(fileId, userId, workspaceId))) {
+                return null;
+            }
+
+            var file = await _context.Files.SingleAsync(f => f.Id == fileId);
+            return await _fileHelper.DownloadFileAsync(file.SavedName, workspaceId);
+        }
+
+        private async Task<bool> EligibleForTheFileAsync(int fileId, int userId, int workspaceId) {
+            if (!await _context.Files.AnyAsync(f => f.Id == fileId && f.WorkspaceId == workspaceId)) {
+                return false;
+            }
+
+            var messageIds = await _context.MessageFileAttachments
+                .Where(mfa => mfa.FileId == fileId)
+                .Select(mfa => mfa.MessageId)
+                .ToListAsync();
+
+            if (await _context.ChannelMessages
+                .AnyAsync(cm => messageIds.Contains(cm.Id) &&
+                                _context.ChannelSubscriptions.Any(cs => cs.ChannelId == cm.Id && cs.UserId == userId))) {
+                return true;
+            }
+
+            if (await _context.ConversationMessages
+                .AnyAsync(cm => messageIds.Contains(cm.Id) &&
+                                _context.ConversationUsers.Any(cu => cu.ConversationId == cm.Id && cu.UserId == userId))) {
+                return true;
+            }
+
+            return false;
         }
     }
 }
