ada301c7b12eb69033d8402b0743948e54e1d02b
liefu zhang <liefuzhangnz@gmail.com>
2019-07-05 22:15:37 +1200

    extracted file item toolbar component out

--- ichat.react/src/content/ContentMessageItem.FileItem.js
+++ ichat.react/src/content/ContentMessageItem.FileItem.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./ContentMessageItem.FileItem.css";
-import { Icon, Popup } from "semantic-ui-react";
+import ContentMessageItemFileItemToolbar from "./ContentMessageItem.FileItemToolbar";
+import { Icon } from "semantic-ui-react";
 import ApiService from "services/ApiService";
 import { toast } from "react-toastify";
-import ShareFileForm from "modalForms/ShareFileForm";
-import Modal from "modals/Modal";
 
 class ContentMessageItemFileItem extends React.Component {
   constructor(props) {
@@ -74,6 +73,9 @@ class ContentMessageItemFileItem extends React.Component {
               onLoad={this.props.onContentHeightChanged}
               alt={file.name}
             />
+            <div className="file-item-toolbar-container">
+              <ContentMessageItemFileItemToolbar file={file} {...this.props} />
+            </div>
           </div>
         )}
         {!this.isImage && (
@@ -81,46 +83,11 @@ class ContentMessageItemFileItem extends React.Component {
             <div className="file-item">
               <Icon name="file outline" size="large" />
               <span title={file.name}>{file.name}</span>
-              <div className="file-item-toolbar">
-                <Popup
-                  trigger={
-                    <div
-                      className="file-item-toolbar-item"
-                      onClick={() => this.onDownloadClick(file.id, file.name)}
-                    >
-                      <Icon name="cloud download" />
-                    </div>
-                  }
-                  content="Download"
-                  inverted
-                  position="top center"
-                  size="tiny"
+              <div className="file-item-toolbar-container">
+                <ContentMessageItemFileItemToolbar
+                  file={file}
+                  {...this.props}
                 />
-                <Popup
-                  trigger={
-                    <div
-                      className="file-item-toolbar-item"
-                      onClick={this.onShareFileClick}
-                    >
-                      <Icon name="share alternate" />
-                    </div>
-                  }
-                  content="Share"
-                  inverted
-                  position="top center"
-                  size="tiny"
-                />
-                <div>
-                  {this.state.isShareFileModalOpen && (
-                    <Modal onClose={this.onCloseShareFile}>
-                      <ShareFileForm
-                        file={file}
-                        onClose={this.onCloseShareFile}
-                        {...this.props}
-                      />
-                    </Modal>
-                  )}
-                </div>
               </div>
             </div>
           </div>
