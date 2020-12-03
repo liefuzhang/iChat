078f356496a21fa0b37883d438d6cc4286b2700e
Liefu Zhang <Liefu.Zhang@vocusgroup.co.nz>
2020-12-03 20:03:11 +1300

    Deployed to heroku

--- ichat.react/ichat-react.ps1
+++
@@ -1,4 +0,0 @@
-docker image build -t ichatreact . -f .\Dockerfile.prod
-docker tag ichatreact registry.heroku.com/ichat-react/web
-docker push registry.heroku.com/ichat-react/web
-heroku container:release web -a ichat-react
