f26731e8f8cc8bc0019162cd0b7a055eda91d28c
AmberCui1995 <ambercui199528@gmail.com>
2022-04-03 08:35:11 +1200

    Invite people on create channel form

--- ichat.react/src/modalForms/CreateChannelForm.js
+++ ichat.react/src/modalForms/CreateChannelForm.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { Dropdown } from "semantic-ui-react";
 import { toast } from "react-toastify";
 import ApiService from "services/ApiService";
 
 class CreateChannelForm extends React.Component {
   constructor(props) {
     super(props);
-
     this.onCreateChannelFormSubmit = this.onCreateChannelFormSubmit.bind(this);
+    this.changeInviteMembers = this.changeInviteMembers.bind(this);
     this.apiService = new ApiService(props);
+    this.state = {
+      userList: []
+    };
+    this.inviteMembersUserIds = [];
   }
 
   onCreateChannelFormSubmit(event) {
     event.preventDefault();
+    if (!this.inviteMembersUserIds || this.inviteMembersUserIds.length === 0)
+      return;
 
     let button = event.currentTarget.querySelector("button[type='submit']");
     button.classList.add("disabled-button");
@@ -29,12 +36,47 @@ class CreateChannelForm extends React.Component {
       })
       .then(id => {
         this.props.onChannelCreated();
+        let url = `/api/channels/${id}/inviteOtherMembers`;
         this.props.history.push(`/channel/${id}`);
+        this.apiService
+          .fetch(url, {
+            method: "POST",
+            body: JSON.stringify(this.inviteMembersUserIds)
+          })
+          .catch(error => {
+            toast.error(`Invite other members failed: ${error}`);
+            button.classList.remove("disabled-button");
+          });
       })
       .catch(error => {
         toast.error(`Create channel failed: ${error}`);
         button.classList.remove("disabled-button");
       });
+
+  }
+
+
+  changeInviteMembers(event, item) {
+    this.inviteMembersUserIds = item.value;
+  }
+
+  componentDidMount() {
+    let allUsersPromise = this.apiService.fetch("/api/users");
+    let memberIdsPromise = this.apiService.fetch(
+      `/api/${this.props.isChannel ? "channels" : "conversations"}/${this.props.id
+      }/userIds`
+    );
+
+    Promise.all([allUsersPromise, memberIdsPromise]).then(
+      ([users, memberIds]) => {
+        let userList = users
+          .filter(u => memberIds.every(id => id !== u.id))
+          .map(u => {
+            return { key: u.id, text: u.displayName, value: u.id };
+          });
+        this.setState({ userList: userList });
+      }
+    );
   }
 
   render() {
@@ -70,6 +112,20 @@ class CreateChannelForm extends React.Component {
               id="createChannelTopic"
               name="topic"
               placeholder="What is this channel about?"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="createChannelTopic">
+              Topic<span className="secondary-text">&nbsp;Select people you want to invite to this</span>
+            </label>
+            <Dropdown
+              placeholder="Search user"
+              fluid
+              multiple
+              search
+              selection
+              options={this.state.userList}
+              onChange={this.changeInviteMembers}
             />
           </div>
           <button type="submit" className="btn form-control">
